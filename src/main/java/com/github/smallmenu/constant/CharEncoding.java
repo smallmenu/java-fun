4e04ce2dd55bdc6b09d20d11d834b1339534d4e0
niuchaoqun <smallmenu@163.com>
2020-12-22 16:37:07 +0800

    + md5

---
+++ src/main/java/com/github/smallmenu/constant/CharEncoding.java
@@ -0,0 +1,14 @@
+package com.github.smallmenu.constant;
+
+/**
+ * CharEncoding
+ *
+ * @author smallmenu
+ */
+public class CharEncoding {
+    public static final String ISO_8859_1 = "ISO-8859-1";
+
+    public static final String UTF_8 = "UTF-8";
+
+    public static final String GBK = "GBK";
+}
